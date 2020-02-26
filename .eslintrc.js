cc2bc1bcdc0986c382b88f735c5057e0b1bb7fba
João Victor <jvictorsantos852@gmail.com>
2020-02-26 19:56:44 -0300

    feat: configured dev tools
    editorconfig, prettier, eslint, husky, lint-staged and commitlint configured

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".js", ".jsx"]
+      }
+    ],
+    "import/prefer-default-export": "off"
+  }
+};
